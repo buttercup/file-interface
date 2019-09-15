a2ccb1ab85b3e203ac0b5d0dc392882cc15bfc34
Perry Mitchell <perry@perrymitchell.net>
2019-09-15 21:05:13 +0300

    Add fs interface

---
+++ source/interfaces/FSInterface.js
@@ -0,0 +1,82 @@
+const VError = require("verror");
+const joinPath = require("join-path");
+const FileSystemInterface = require("../FileSystemInterface.js");
+
+module.exports = class FSInterface extends FileSystemInterface {
+    constructor(fs) {
+        super();
+        this.fs = fs;
+    }
+
+    getDirectoryContents(pathIdentifier) {
+        const { identifier: dir } = pathIdentifier;
+        const fetch = new Promise((resolve, reject) => {
+            this.fs.readdir(dir, (err, filenames) => {
+                if (err) {
+                    return reject(new VError(err, `Error reading directory contents: ${dir}`));
+                }
+                resolve(filenames);
+            });
+        });
+        return fetch.then(filenames => this._filenamesToStats(dir, filenames));
+    }
+
+    getFileContents(pathIdentifier) {
+        const { identifier: filename } = pathIdentifier;
+        return new Promise((resolve, reject) => {
+            this.fs.readFile(filename, "utf8", (err, data) => {
+                if (err) {
+                    return reject(new VError(err, `Error reading file: ${filename}`));
+                }
+                resolve(data);
+            });
+        });
+    }
+
+    getSupportedFeatures() {
+        return ["created", "modified"];
+    }
+
+    putFileContents(parentPathIdentifier, fileIdentifier, data) {
+        const filename = joinPath(parentPathIdentifier.identifier, fileIdentifier.identifier);
+        return new Promise((resolve, reject) => {
+            this.fs.writeFile(filename, data, err => {
+                if (err) {
+                    return reject(new VError(err, `Error writing file: ${filename}`));
+                }
+                resolve(fileIdentifier);
+            });
+        });
+    }
+
+    _filenamesToStats(dir, filenames) {
+        return Promise.all(filenames.map(filename => this._stat(dir, filename)));
+    }
+
+    _stat(dir, filename) {
+        const fullPath = joinPath(dir, filename);
+        return new Promise((resolve, reject) => {
+            this.fs.stat(fullPath, (err, stat) => {
+                if (err) {
+                    return reject(new VError(err, `Failed getting file properties: ${filename}`));
+                }
+                const {
+                    ctime,
+                    mtime,
+                    size
+                } = stat;
+                const type = stat.isDirectory() ? "directory" : "file";
+                const created = new Date(ctime).toUTCString();
+                const modified = new Date(mtime).toUTCString();
+                resolve({
+                    identifier: fullPath,
+                    name: filename,
+                    type,
+                    size,
+                    created,
+                    modified
+                });
+            });
+        });
+    }
+};
