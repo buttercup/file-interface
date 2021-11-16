188770f0e968a72a95b6788803890f6b81fac93a
Perry Mitchell <perry@perrymitchell.net>
2021-11-16 22:58:16 +0200

    Prepare 1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # File Interface changelog
 
+## v1.0.1
+_2021-11-16_
+
+ * Export types
+
 ## v1.0.0
 _2021-11-16_
 
--- source/index.ts
+++ source/index.ts
@@ -4,3 +4,5 @@ export { GoogleDriveInterface } from "./interfaces/GoogleDriveInterface";
 export { NodeFSInterface } from "./interfaces/NodeFSInterface";
 export { WebDAVInterface } from "./interfaces/WebDAVInterface";
 export { getInterfaceReference, instantiateInterface, registerInterface } from "./register";
+
+export * from "./types";
